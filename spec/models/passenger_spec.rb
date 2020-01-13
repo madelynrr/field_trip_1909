47916ff79c3f2103cfd49fe42d5274cd7e8cec58
Madelyn Romero <m.romero013@gmail.com>
2020-01-13 09:52:10 -0700

    Add test for many to many with flights

--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -8,5 +8,6 @@ RSpec.describe Passenger, type: :model do
 
   describe "relationships" do
     it {should have_many :flight_passengers}
+    it {should have_many(:flights).through(:flight_passengers)}
   end
 end
