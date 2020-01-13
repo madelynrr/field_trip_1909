f5fa006778054364e65337d744c10207fad5daae
Madelyn Romero <m.romero013@gmail.com>
2020-01-13 10:07:18 -0700

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+airline = Airline.create(name: "Southwest")
+flight = airline.flights.create(number: "714",
+                                date: "10/14/19",
+                                time: "10:30 AM",
+                                departure_city: "Tempe",
+                                arrival_city: "Denver")
+passenger_1 = Passenger.create(name: "Bob",
+                               age: 20)
+passenger_2 = Passenger.create(name: "Sally",
+                               age: 30)
+passenger_3 = Passenger.create(name: "Billy",
+                               age: 40)
+flight.passengers << [passenger_1, passenger_2]
