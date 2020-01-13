c043f4e9b111430c9ae3873fb3285d5f690f9fe0
Madelyn Romero <m.romero013@gmail.com>
2020-01-13 09:26:37 -0700

    Set up many to many relationship for passengers

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,5 +2,8 @@ class Passenger < ApplicationRecord
 
   validates_presence_of :name
   validates_presence_of :age
-  
+
+  has_many :airline_passengers
+  has_many :airlines, through: :airline_passengers
+
 end
