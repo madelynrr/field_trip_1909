c494f4e0997bd7d92c0442eea9c6c896e8020359
Madelyn Romero <m.romero013@gmail.com>
2020-01-13 09:23:25 -0700

    Add test for many to many relationship with airlines

--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Passenger, type: :model do
     it {should validate_presence_of :name}
     it {should validate_presence_of :age}
   end
+
+  describe "relationships" do
+    it {should have_many :airline_passengers}
+    # it {should belong_to(:airline).through(:airline_passengers)}
+  end
 end
