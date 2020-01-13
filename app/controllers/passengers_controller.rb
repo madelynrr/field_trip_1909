08126c82a33a5b8c05e00eab2f5a8e35ce70f31a
Madelyn Romero <m.romero013@gmail.com>
2020-01-13 10:41:11 -0700

    Add update action for adding flight to passenger

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -4,4 +4,11 @@ class PassengersController < ApplicationController
     @passenger = Passenger.find(params[:id])
   end
 
+  def update
+    flight = Flight.where(number: params[:number])
+    passenger = Passenger.find(params[:id])
+    passenger.flights << flight
+    redirect_to "/passengers/#{passenger.id}"
+  end
+
 end
