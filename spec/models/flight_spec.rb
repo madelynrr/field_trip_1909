aea6cc8c9349f8510b22b97e8838fe989f86903e
Madelyn Romero <m.romero013@gmail.com>
2020-01-13 10:58:15 -0700

    Add test for number of adutls model method

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -33,5 +33,23 @@ RSpec.describe Flight, type: :model do
 
       expect(flight.number_of_minors).to eq([passenger_1])
     end
+
+    it ".number_of_adults" do
+      airline = Airline.create(name: "Southwest")
+      flight = airline.flights.create(number: "714",
+                                      date: "10/14/19",
+                                      time: "10:30 AM",
+                                      departure_city: "Tempe",
+                                      arrival_city: "Denver")
+      passenger_1 = Passenger.create(name: "Bob",
+                                     age: 16)
+      passenger_2 = Passenger.create(name: "Sally",
+                                     age: 30)
+      passenger_3 = Passenger.create(name: "Billy",
+                                     age: 40)
+      flight.passengers << [passenger_1, passenger_2, passenger_3]
+
+      expect(flight.number_of_adults).to eq(2)
+    end
   end
 end
