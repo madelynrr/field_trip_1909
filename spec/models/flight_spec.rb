3b912b3967addee9592ca22a4f2cbfab325ab054
Madelyn Romero <m.romero013@gmail.com>
2020-01-13 10:54:44 -0700

    Create model method for array of minors on flight

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -8,9 +8,30 @@ RSpec.describe Flight, type: :model do
     it {should validate_presence_of :departure_city}
     it {should validate_presence_of :arrival_city}
   end
+
   describe 'relationships' do
     it {should belong_to :airline}
     it {should have_many :flight_passengers}
     it {should have_many(:passengers).through(:flight_passengers)}
+  end
+
+  describe "methods" do
+    it ".number_of_minors" do
+      airline = Airline.create(name: "Southwest")
+      flight = airline.flights.create(number: "714",
+                                      date: "10/14/19",
+                                      time: "10:30 AM",
+                                      departure_city: "Tempe",
+                                      arrival_city: "Denver")
+      passenger_1 = Passenger.create(name: "Bob",
+                                     age: 16)
+      passenger_2 = Passenger.create(name: "Sally",
+                                     age: 30)
+      passenger_3 = Passenger.create(name: "Billy",
+                                     age: 40)
+      flight.passengers << [passenger_1, passenger_2, passenger_3]
+
+      expect(flight.number_of_minors).to eq(1)
+    end
   end
 end
